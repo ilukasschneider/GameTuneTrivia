c601aaf3980f475351043b184fd07d8efee224c7
Lukas Schneider <lukasschneiderprivat@icloud.com>
2024-04-13 19:12:32 +0200

    Update page.tsx

--- app/archive/page.tsx
+++ app/archive/page.tsx
@@ -89,14 +89,10 @@ const projects = [
   },
 ];
 
-export function CardHoverEffectDemo() {
+export default function Page() {
   return (
     <div className="max-w-5xl mx-auto px-8">
       <HoverEffect items={projects} />
     </div>
   );
-}
-
-export default function Page() {
-  return <CardHoverEffectDemo />;
 }
