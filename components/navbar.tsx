1e39390689e6d9a683f2d373a60da5e18a35a5be
Lukas Schneider <lukasschneiderprivat@icloud.com>
2024-04-15 23:54:05 +0200

    added own yt-vid for test

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 
 export function Navbar() {
   return (
-    <div className="bg-white sticky top-12 bottom-12 flex flex-col min-h-[12dvh] dark:bg-background justify-center items-center">
+    <div className="bg-white sticky top-12 bottom-12 flex flex-col min-h-[12dvh] dark:bg-background justify-center items-center z-50">
       <nav className="flex flex-col lg:flex-row gap-8 sm:gap-6  items-center lg:items-start">
         <Button variant="ghost" asChild>
           <Link href="/today">Todays Tune</Link>
--- components/ui/audio-player/tune-player.tsx
+++ components/ui/audio-player/tune-player.tsx
@@ -4,7 +4,7 @@ export function TunePlayer() {
   return (
     <div className="video-container mb-40 mt-12 flex flex-col lg:flex-row items-center justify-center relative top-20 w-full gap-4 mx-auto px-14">
       <iframe
-        src="https://www.youtube.com/embed/13U4oqAZy8U?controls=0&showinfo=0&rel=0"
+        src="https://www.youtube.com/embed/CXLLbY3otes?controls=0&showinfo=0&rel=0"
         referrerPolicy="strict-origin-when-cross-origin"
       ></iframe>
     </div>
