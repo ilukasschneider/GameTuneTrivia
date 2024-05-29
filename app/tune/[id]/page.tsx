6ad252f6c8f0a929f0a6de1cdd65d7862cbb50d0
Lukas Schneider <lukasschneiderprivat@icloud.com>
2024-05-29 14:29:25 +0200

    fixed LocalStorage

--- app/tune/[id]/page.tsx
+++ app/tune/[id]/page.tsx
@@ -3,25 +3,38 @@ import GameSearchbar from "@/components/ui/game-ui/game-searchbar";
 import SoundVisualizer from "@/components/ui/game-ui/sound-visualizer";
 import { Button } from "@/components/ui/button";
 import { getTuneData } from "@/lib/db/db-utils";
-import { Suspense, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { YTPlayer } from "@/components/ui/audio-player/yt-player";
 
 export default function Tune({ params }: { params: { id: string } }) {
-  // State for tracking the audio time based on user progression
+  // State to track the user's progress
+  const [progess, setProgress] = useState(
+    typeof window !== "undefined"
+      ? localStorage.getItem(`levelProgression: ${params.id}`) || "0"
+      : "0",
+  );
+
+  // State to track the current audio time
   const [audioTime, setAudioTime] = useState(updateAudioTime());
-  // Initialize user level progression in localStorage if not present
-  if (!localStorage.getItem(`levelProgression: ${params.id}`)) {
+
+  // Effect to initialize the progress in local storage
+  useEffect(() => {
     localStorage.setItem(`levelProgression: ${params.id}`, "0");
-  }
-  // Fetch tune data from the database
+  }, []);
+
+  // Effect to update the progress in local storage whenever it changes
+  useEffect(() => {
+    localStorage.setItem(`levelProgression: ${params.id}`, progess);
+  }, [progess, params.id]);
+
+  // Fetching the tune data based on the id
   const tuneData = getTuneData(params.id)!;
-  const correctGameIDs = tuneData.game_ids; // IDs of games that are correct answers
-  const audio = `/static/audio/${tuneData.id}/audio.mp3`; // Path to the tune audio file
-  // State to track the user's selected game ID
+  const correctGameIDs = tuneData.game_ids;
+  const audio = `/static/audio/${tuneData.id}/audio.mp3`;
   const [selectedGameID, setSelectedGameID] = useState(-1);
 
-  // Function to update the audio time based on the user's current progression
+  // Function to update the audio time based on the progress
   function updateAudioTime(): string {
     const progressionMap: { [key: string]: string } = {
       "0": "5",
@@ -30,34 +43,26 @@ export default function Tune({ params }: { params: { id: string } }) {
       "3": "15",
       "4": "15",
     };
-    const currentProgression =
-      localStorage.getItem(`levelProgression: ${params.id}`) || "0";
-    return progressionMap[currentProgression] || "5";
+
+    return progressionMap[progess] || "5";
   }
 
-  // Function to check the user's guess and update their progression
+  // Function to check the user's guess
   function checkGuess() {
-    const currentProgression =
-      localStorage.getItem(`levelProgression: ${params.id}`) || "0";
+    const currentProgression = progess;
     if (selectedGameID != -1) {
       if (
         correctGameIDs.includes(selectedGameID) &&
         currentProgression !== "3"
       ) {
-        // If guess is correct and not at max progression, set to completed
-        localStorage.setItem(`levelProgression: ${params.id}`, "4");
+        setProgress("4");
       } else {
-        // Otherwise, increment progression or set to 'done' if already completed
         const nextProgression = Math.min(
           parseInt(currentProgression) + 1,
           3,
         ).toString();
-        localStorage.setItem(
-          `levelProgression: ${params.id}`,
-          currentProgression === "done" ? "done" : nextProgression,
-        );
+        setProgress(currentProgression === "4" ? "4" : nextProgression);
       }
-      // Update the audio time based on new progression
       setAudioTime(updateAudioTime());
     }
   }
@@ -65,10 +70,8 @@ export default function Tune({ params }: { params: { id: string } }) {
   return (
     <>
       <div className="place-content-center grid gap-3">
-        {localStorage.getItem(`levelProgression: ${params.id}`) !== "3" &&
-        localStorage.getItem(`levelProgression: ${params.id}`) !== "4" ? (
+        {progess !== "3" && progess !== "4" ? (
           <Suspense fallback={<div>Loading...</div>}>
-            {/* Sound visualizer component with dynamic audio length */}
             <SoundVisualizer audio={audio} length={parseInt(audioTime)} />
           </Suspense>
         ) : (
@@ -78,26 +81,14 @@ export default function Tune({ params }: { params: { id: string } }) {
 
       <div className="place-content-center grid gap-3">
         <div className="flex justify-center gap-10 mb-8 -mt-4">
-          {/* Example avatars */}
-          <Avatar className="outline-inherit">
-            <AvatarImage />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-          <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png" alt="" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-          <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
+          <div className="w-8 h-8 bg-secondary rounded-full" />
+          <div className="w-8 h-8 bg-secondary rounded-full" />
+          <div className="w-8 h-8 bg-secondary rounded-full" />
         </div>
         <GameSearchbar setGameID={setSelectedGameID} />
         <Button variant={"secondary"} onClick={checkGuess}>
           Guess
         </Button>
-        <p>selected game: {selectedGameID}</p>
-        <p>current time: {audioTime}</p>
       </div>
     </>
   );
--- components/ui/audio-player/yt-player.tsx
+++ components/ui/audio-player/yt-player.tsx
@@ -4,7 +4,7 @@ export function YTPlayer({ video_id }: { video_id: string }) {
   return (
     <div className="video-container mb-40 mt-12 flex flex-col lg:flex-row items-center justify-center relative top-20 w-full gap-4 mx-auto px-14">
       <iframe
-        src={`https://www.youtube.com/embed/${video_id}`}
+        src={`https://www.youtube.com/embed/${video_id}?controls=0&showinfo=0&rel=0&autoplay=1&playsinline=1`}
         referrerPolicy="strict-origin-when-cross-origin"
       ></iframe>
     </div>
