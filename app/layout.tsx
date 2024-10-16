4c823350dc58e5353b3066f3fd08b6167db7f3ca
Lukas Schneider <lukasschneiderprivat@icloud.com>
2024-10-17 00:46:54 +0200

    trying again

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,13 +13,14 @@ import { Navbar } from "@/components/navbar";
 export const metadata: Metadata = {
   title: "Game Tunes Trivia",
   description: "Where Gaming Meets Melody",
+  viewport: "width=device-width, initial-scale=1",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body
