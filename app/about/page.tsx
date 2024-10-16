311317a2e0dc95e4eba5a8d616847f5f0b5f3da9
Lukas Schneider <lukasschneiderprivat@icloud.com>
2024-10-17 01:48:01 +0200

    fixed text

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,17 +4,14 @@ export default function About() {
       <div className="container mx-auto max-w-3xl">
         <div className="space-y-6">
           <h1 className="text-4xl font-bold tracking-tight sm:text-5xl lg:text-6xl">
-            Embark on an Adventure with Game Tune Trivia
+            Game Tune Trivia
           </h1>
-          <h2 className="text-2xl font-semibold tracking-tight sm:text-3xl">
-            Explore the Fusion of Gaming and Music
-          </h2>
           <div className="prose prose-lg dark:prose-invert">
             <p>
-              Dive into Game Tune Trivia, a project born from my first journey
-              into Next.js and full-stack development. This is my first attempt
-              at creating a web app, blending my passion for video game
-              soundtracks with the thrill of learning something new.
+              This is a project born from my first journey into Next.js and
+              full-stack development. This is my first attempt at creating a web
+              app, blending my passion for video game soundtracks with the
+              thrill of learning something new.
             </p>
           </div>
         </div>
